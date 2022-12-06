16ccc4e370750bc8a2340ca9c8d31bf96131ceff
hero <ghnm89@gmail.com>
2022-12-07 00:11:22 +0900

    :sparkles: bootstrap template 추가

--- src/main/java/com/root/study/controller/MainController.java
+++ src/main/java/com/root/study/controller/MainController.java
@@ -3,6 +3,7 @@ package com.root.study.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
@@ -11,6 +12,13 @@ public class MainController {
     public String mainText(Model model) {
         model.addAttribute("data", "Hello ThymeLeaf");
 
-        return "index";
+        return "index_test";
+    }
+
+    @GetMapping("/main")
+    public ModelAndView main() {
+        ModelAndView mnv = new ModelAndView();
+        mnv.setViewName("index");
+        return mnv;
     }
 }
